156336f39480d47c6e89dde9f3cde58532af2b28
Bimaaaaaa <bmarzky.official@gmail.com>
2025-11-22 01:18:27 +0700

    update icon web

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bima - Portofolio</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="assets/images/profile.png" type="image/png">
+    <link rel="icon" href="assets/images/bima.png" type="image/png">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
