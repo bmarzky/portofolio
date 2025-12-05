4564c4011d6869fa3d142407df8bc345b55354ec
Bimaaaaaa <bmarzky.official@gmail.com>
2025-12-05 16:14:58 +0700

    Refactor: title name

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Sidebar</title>
+    <title>Bima - Portofolio</title>
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="assets/images/bima.png">
     <link rel="stylesheet" href="assets/css/style.css">
